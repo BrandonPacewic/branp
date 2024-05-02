624e5a9825850fd0ff1cebeb93fb0f82a71df5a0
BrandonPacewic <pacewicbrandon@gmail.com>
2024-05-01 21:25:17 -0700

    Import logging

--- branp/__init__.py
+++ branp/__init__.py
@@ -2,8 +2,11 @@
 # SPDX-License-Identifier: MIT
 
 from branp.core import OS
+from branp.logging import init_logging
 
 assert OS == "Darwin", "All current development has been on OSX. Other platform behavior is undefined."
 
 __version__ = "v0.0.b0"
 __all__ = ["__version__"]
+
+init_logging()
