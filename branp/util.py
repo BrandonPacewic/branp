5f186cddade5ca385b65f63b11451637132dc0bc
BrandonPacewic <pacewicbrandon@gmail.com>
2024-07-07 23:22:57 -0700

    Added gereal timing class

--- branp/util.py
+++ branp/util.py
@@ -1,6 +1,7 @@
 # Copyright (c) Brandon Pacewic
 # SPDX-License-Identifier: MIT
 
+from time import perf_counter
 from typing import List
 
 GREEN = "\033[92m"
@@ -8,6 +9,20 @@ RED = "\033[91m"
 BOLD_RED = "\033[1;91m"
 YELLOW = "\033[93m"
 ENDC = "\033[0m"
+
+
+class Timer:
+    def __init__(self) -> None:
+        self.tics = [perf_counter()]
+
+    def add_tic(self) -> None:
+        self.tics.append(perf_counter())
+
+    def get_elapsed(self) -> str:
+        try:
+            return f"{self.tics[-1] - self.tics[-2]:.3f}s"
+        except IndexError:
+            return "-0.000s"
 
 
 def get_similar(key: str, match_against: List[str]) -> str | None:
