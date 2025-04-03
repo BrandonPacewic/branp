d8953627d5aa401c2a206885178e2c84cd20068e
BrandonPacewic <92102436+BrandonPacewic@users.noreply.github.com>
2025-04-03 13:13:52 -0700

    feat(format): support multiple formatters

--- src/commands/format.rs
+++ src/commands/format.rs
@@ -24,8 +24,6 @@ use std::process::Command as ProcessCommand;
 
 use crate::context::GlobalContext;
 
-const FILE_TARGETS: [&str; 7] = ["cpp", "h", "cc", "hpp", "cxx", "c", "cs"];
-
 pub fn command() -> Command {
     Command::new("format")
         .about("Format a C++ file using clang-format.")
@@ -41,49 +39,65 @@ pub fn command() -> Command {
         )
 }
 
-pub fn exec(gctx: &mut GlobalContext, args: &ArgMatches) {
-    gctx.shell().note("Indexing...");
+struct FormatCollection<'a> {
+    files: Vec<PathBuf>,
+    formatter: &'a CodeFormatter,
+}
 
-    let config = get_clang_format_config(gctx, args);
-    let mut files: Vec<PathBuf> = Vec::new();
-    let paths = vec![PathBuf::from(".")];
+pub fn exec(gctx: &mut GlobalContext, _args: &ArgMatches) {
+    let entries = fs::read_dir(".").unwrap_or_else(|_| panic!("Failed to read current directory"));
+    let formatters = get_formatters(entries, vec![]);
 
-    for path in paths {
-        if path.is_dir() {
-            walk_directory_for_files(&path, &mut files);
-        }
-    }
+    for fc in formatters {
+        gctx.shell().note(format!(
+            "Formatting {} file(s) with {}...",
+            fc.files.len(),
+            fc.formatter.name
+        ));
+        for file in fc.files {
+            let mut command = (fc.formatter.runner)(&file, None);
+            let output = command.output().unwrap_or_else(|_| {
+                panic!("Failed to execute formatter for file: {}", file.display())
+            });
 
-    gctx.shell()
-        .note(format!("Formatting {} file(s)...", files.len()));
-
-    for file in files {
-        let mut command = ProcessCommand::new("clang-format");
-        command.arg("-i");
-        if let Some(config) = &config {
-            command.arg(format!("--style=file:{}", config));
-        } else {
-            command.arg("--style=file");
-        }
-
-        command.arg(&file);
-        let output = command.output();
-        if let Ok(output) = output {
             if !output.status.success() {
                 gctx.shell().error(format!(
                     "Failed to format file: {}",
                     String::from_utf8_lossy(&output.stderr)
                 ));
             }
-        } else {
-            gctx.shell().error(format!(
-                "Failed to execute clang-format for file: {}",
-                file.display()
-            ));
         }
     }
 
     gctx.shell().note("Done!");
+}
+
+fn get_formatters(
+    entries: fs::ReadDir,
+    mut formatters: Vec<FormatCollection>,
+) -> Vec<FormatCollection> {
+    for entry in entries.filter_map(Result::ok) {
+        let path = entry.path();
+        if path.is_dir() {
+            let sub_entries =
+                fs::read_dir(&path).unwrap_or_else(|_| panic!("Failed to read directory"));
+            formatters = get_formatters(sub_entries, formatters);
+        } else if let Some(formatter) = get_code_formatter(&path) {
+            if let Some(created_formatter) = formatters
+                .iter_mut()
+                .find(|f| f.formatter.name == formatter.name)
+            {
+                created_formatter.files.push(path);
+            } else {
+                formatters.push(FormatCollection {
+                    files: vec![path],
+                    formatter,
+                });
+            }
+        }
+    }
+
+    formatters
 }
 
 fn get_clang_format_config(gctx: &mut GlobalContext, args: &ArgMatches) -> Option<String> {
@@ -125,17 +139,36 @@ fn get_clang_format_config(gctx: &mut GlobalContext, args: &ArgMatches) -> Optio
     config_file
 }
 
-fn walk_directory_for_files(dir: &Path, files: &mut Vec<PathBuf>) {
-    if let Ok(entries) = fs::read_dir(dir) {
-        for entry in entries.filter_map(Result::ok) {
-            let path = entry.path();
-            if path.is_dir() {
-                walk_directory_for_files(&path, files);
-            } else if let Some(ext) = path.extension().and_then(|s| s.to_str()) {
-                if FILE_TARGETS.contains(&ext) {
-                    files.push(path);
-                }
-            }
-        }
+struct CodeFormatter {
+    name: &'static str,
+    valid_extensions: &'static [&'static str],
+    runner: fn(&PathBuf, Option<String>) -> ProcessCommand,
+}
+
+fn clang_format_command(file: &PathBuf, config: Option<String>) -> ProcessCommand {
+    let mut command = ProcessCommand::new("clang-format");
+    command.arg("-i");
+    if let Some(config) = config {
+        command.arg(format!("--style=file:{}", config));
+    } else {
+        command.arg("--style=file");
     }
+
+    command.arg(file);
+    command
+}
+
+const CLANG: CodeFormatter = CodeFormatter {
+    name: "clang-format",
+    valid_extensions: &["cpp", "h", "cc", "hpp", "cxx", "c", "cs"],
+    runner: clang_format_command,
+};
+
+const FORMATTERS: [CodeFormatter; 1] = [CLANG];
+
+fn get_code_formatter(file: &PathBuf) -> Option<&'static CodeFormatter> {
+    let ext = file.extension()?.to_str()?;
+    FORMATTERS
+        .iter()
+        .find(|formatter| formatter.valid_extensions.contains(&ext))
 }
